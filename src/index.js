e638f2c5de15a458c3b740c74cc11cf00573aba6
Ahmad Nassri <ahmad@codeinchaos.com>
2015-04-16 02:37:55 -0400

    tests + coverage + readme

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 'use strict'
 
+var assert = require('assert')
+var common = require('winston/lib/winston/common')
+var debug = require('debug')('winston:tcp')
+var EntryBuffer = require('./buffer')
 var net = require('net')
 var util = require('util')
 var winston = require('winston')
-var common = require('winston/lib/winston/common')
 
 var TCP = winston.transports.TCP = function (options) {
+  options = options || {}
   winston.Transport.call(this, options)
 
-  this.options = options || {}
+  assert(options.host, 'Must define a host')
+  assert(options.port, 'Must supply a port')
 
   // generic transport requirements
   this.name = 'winston-tcp'
   this.level = options.level || 'info'
 
   // tcp transport specific options
-  this.host = options.host || '127.0.0.1'
-  this.port = options.port || 1337
+  this.host = options.host
+  this.port = options.port
+
+  this.timestamp = options.timestamp
+  this.json = options.json
+  this.reconnectInterval = options.reconnectInterval || 1000
+
+  // initiate entry buffer
+  this.entryBuffer = new EntryBuffer(options.bufferLength || 10000)
+
+  // internal flags
+  this.connected = false
+  this.connectionAttempts = 0 // cleared after each connection
+  this.connectionCount = 0
+  this.reconnect = false
 
   this.connect()
 }
@@ -24,36 +42,86 @@ var TCP = winston.transports.TCP = function (options) {
 util.inherits(TCP, winston.Transport)
 
 TCP.prototype.connect = function () {
-  this.client = new net.Socket()
-  this.client.connect(this.port, this.host)
+  if (!this.connected) {
+    debug('connection attempt #%s', ++this.connectionAttempts)
+
+    this.reconnect = true
+    this.socket = new net.Socket()
+    this.socket.unref()
+
+    this.socket.on('error', function (err) {
+      debug('socket error %j', err)
+    })
+
+    this.socket.on('connect', function () {
+      this.connected = true
+      this.connectionAttempts = 0
+
+      debug('connection established #%s', ++this.connectionCount)
+
+      // attempt to resend messages
+
+      var bufferLength = this.entryBuffer.length()
+
+      if (bufferLength) {
+        debug('draining buffer of %s entries', bufferLength)
+
+        this.entryBuffer.drain(this.write.bind(this))
+      }
+    }.bind(this))
+
+    this.socket.on('close', function () {
+      debug('connection closed')
+
+      this.socket.destroy()
+      this.connected = false
+
+      if (this.reconnect) {
+        debug('attempt to reconnect in %s', this.reconnectInterval)
+        setTimeout(this.connect.bind(this), this.reconnectInterval)
+      }
+    }.bind(this))
+
+    this.socket.connect(this.port, this.host)
+  }
+}
+
+TCP.prototype.disconnect = function () {
+  this.connected = false
+  this.reconnect = false
+  this.socket.end()
+}
+
+TCP.prototype.write = function (entry, callback) {
+  if (this.connected) {
+    debug('writing to socket %j', entry)
+
+    this.socket.write(entry, 'utf8', function () {
+      if (callback) callback(null, true)
+    })
+  } else {
+    debug('writing to buffer %j', entry)
+
+    this.entryBuffer.add(entry)
+    if (callback) callback(null, true)
+  }
 }
 
 TCP.prototype.log = function (level, msg, meta, callback) {
-  var self = this
-
   if (typeof meta === 'function') {
     callback = meta
     meta = {}
   }
 
-  console.log(self.options)
-
   var entry = common.log({
     level: level,
     message: msg,
     meta: meta,
-    timestamp: self.options.timestamp || false,
-    json: self.options.json || false
+    timestamp: this.timestamp,
+    json: this.json
   })
 
-  console.log(self.options.json)
-
-  self.client.write(entry, 'utf8', function () {
-    self.emit('logged')
-
-    if (callback) callback(null, true)
-  })
-
+  this.write(entry, callback)
 }
 
 module.exports = TCP
--- test/index.js
+++ test/index.js
@@ -1,32 +1,154 @@
-/* global describe, it */
+/* global describe, it, beforeEach, afterEach */
 
 'use strict'
 
-require('..')
-
+var debug = require('debug')('winston:tcp:test')
+var net = require('net')
+var should = require('should')
+var Transport = require('..')
 var winston = require('winston')
 
-require('should')
+var server
 
-describe('node module', function () {
-  it('must have at least one test', function (done) {
-    var logger = new (winston.Logger)({
-      transports: [
-        new (winston.transports.Console)(),
-        new (winston.transports.TCP)({
-          host: '127.0.0.1',
-          port: 1337,
-          json: true,
-          timestamp: true
-        })
-      ]
+beforeEach(function (done) {
+  server = net.createServer(function (socket) {
+    socket.on('data', function (data) {
+      debug('[%s]: %s', new Date().toISOString(), data.toString())
     })
 
-    logger.log('info', { data: 'foo'})
-    logger.log('info', 'foo')
+    socket.on('error', function (err) {
+      debug('[%s] %s:', new Date().toISOString(), err.toString())
+    })
+  })
 
-    true.should.be.true
+  server.listen(1337, '127.0.0.1', 10, done)
+})
+
+afterEach(function () {
+  server.close()
+})
+
+describe('node module', function () {
+  it('should fail if no host & port are provided', function (done) {
+    var transport
+
+    /*eslint-disable no-wrap-func */
+    (function () {
+      transport = new Transport()
+    }).should.throw(Error)
+
+    should.not.exist(transport)
 
     done()
+  })
+
+  it('should try reconnecting', function (done) {
+    var transport = new Transport({
+      host: '0.0.0.0',
+      port: 1330,
+      reconnectInterval: 50
+    })
+
+    setTimeout(function () {
+      // set the correct port
+      transport.port = 1337
+      transport.connectionAttempts.should.equal(4)
+    }, 200)
+
+    setTimeout(function () {
+      done()
+    }, 500)
+  })
+
+  it('should close connection', function (done) {
+    var transport = new Transport({
+      host: '0.0.0.0',
+      port: 1337,
+      reconnectInterval: 50
+    })
+
+    setTimeout(function () {
+      transport.connected.should.be.true
+    }, 100)
+
+    setTimeout(function () {
+      transport.disconnect()
+      transport.connected.should.be.false
+      done()
+    }, 200)
+  })
+
+  it('should buffer entries', function (done) {
+    var data = Array.apply(null, {length: 20}).map(Math.random)
+
+    var transport = new Transport({
+      host: '0.0.0.0',
+      port: 1330
+    })
+
+    var logger = new winston.Logger({
+      transports: [transport]
+    })
+
+    data.forEach(function (msg) {
+      logger.log('info', msg)
+    })
+
+    transport.entryBuffer.length().should.equal(20)
+
+    done()
+  })
+
+  it('should write entries', function (done) {
+    var data = Array.apply(null, {length: 20}).map(Math.random)
+
+    var transport = new Transport({
+      host: '0.0.0.0',
+      port: 1337
+    })
+
+    var logger = new winston.Logger({
+      transports: [transport]
+    })
+
+    // delay a bit to allow socket connection
+    setTimeout(function () {
+      data.forEach(function (msg) {
+        logger.log('info', msg)
+      })
+
+      transport.entryBuffer.length().should.equal(0)
+
+      done()
+    }, 100)
+  })
+
+  it('should buffer first then drain when connection is established', function (done) {
+    var data = Array.apply(null, {length: 20}).map(Math.random)
+
+    var transport = new Transport({
+      host: '0.0.0.0',
+      port: 1330,
+      reconnectInterval: 200
+    })
+
+    var logger = new winston.Logger({
+      transports: [transport]
+    })
+
+    data.forEach(function (msg) {
+      logger.log('info', msg)
+    })
+
+    setTimeout(function () {
+      // set the correct port
+      transport.port = 1337
+    }, 100)
+
+    setTimeout(function () {
+      transport.entryBuffer.length().should.equal(0)
+
+      done()
+    }, 400)
   })
 })
